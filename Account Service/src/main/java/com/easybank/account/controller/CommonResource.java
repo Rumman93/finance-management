f180e9fe9e24f4dcbb29ba3e4104c014dd45b879
Rumman93 <ahmmedjubayer2015@gmail.com>
2024-10-05 12:37:09 +0600

    added common resource class

---
+++ Account Service/src/main/java/com/easybank/account/controller/CommonResource.java
@@ -0,0 +1,11 @@
+package com.easybank.account.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Author :  Ahmmed Jubayer Rumman
+ * Created at : 05 Oct 2024
+ */
+@RestController
+public class CommonResource {
+}
